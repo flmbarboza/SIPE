67618a6094dc71fdaddf2823cdb46239ac4155f1
Flavio Barboza <flmbarboza@gmail.com>
2026-07-10 21:51:06 -0300

    Create bsc_perspective.py

---
+++ domain/enums/bsc_perspective.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class BSCPerspective(str, Enum):
+
+    FINANCIAL = "Financeira"
+
+    CUSTOMER = "Clientes"
+
+    INTERNAL_PROCESS = "Processos Internos"
+
+    LEARNING = "Aprendizado e Crescimento"
+
+    SUSTAINABILITY = "Sustentabilidade"
